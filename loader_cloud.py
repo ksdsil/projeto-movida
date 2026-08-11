4251e7e39fa9fd715db3cf9304484c89d2b8b1fd
ksdsil <89704209+ksdsil@users.noreply.github.com>
2026-08-11 09:25:50 -0300

    Update comments and connection string instructions

--- loader_cloud.py
+++ loader_cloud.py
@@ -2,18 +2,18 @@ import pandas as pd
 from sqlalchemy import create_engine
 import os
 
-# ==========================================
-# CONFIGURAÇÃO DA NUVEM (SUPABASE)
-# Cole aqui a sua String de Conexão do Supabase
+
+# CONFIGURAÇÃO DA NUVEM NESTE CASO SUPABASE SE MUDAR PRECISA POR OUTRO
+# Tem que aqui a String de Conexão 
 DATABASE_URL = os.environ.get("DB_URL")
-# ==========================================
+
 
 def carregar_csv_silver():
     caminho_csv = "silver/veiculos_clean.csv"
     if not os.path.exists(caminho_csv):
         print("[!] Arquivo CSV da camada silver não encontrado.")
         return None
-    # O pandas é a forma mais pythônica e eficiente de ler e inserir dados em massa
+   
     df = pd.read_csv(caminho_csv)
     print(f"[*] CSV carregado. {len(df)} registros prontos para a nuvem.")
     return df
@@ -28,7 +28,7 @@ def enviar_para_nuvem(df):
         engine = create_engine(DATABASE_URL)
         
         print("[*] Enviando dados para a tabela 'veiculos_silver'...")
-        # Envia o dataframe inteiro para o SQL. Se a tabela existir, ela será substituída.
+        # Envia o dataframe inteiro para o SQL. Se a tabela existir, ela vai ser substituída.
         df.to_sql("veiculos_silver", engine, if_exists="replace", index=False)
         
         print("\n[+] DADOS ENVIADOS PARA A NUVEM COM SUCESSO!")
